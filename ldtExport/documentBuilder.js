0219bc67ee058b944ee6bcddde56ddd39043560d
aluther93 <alexander_luther@web.de>
2019-10-02 12:24:22 +0200

    präsentation

--- ldtExport/documentBuilder.js
+++ ldtExport/documentBuilder.js
@@ -99,6 +99,8 @@ function returnFalse(){
 }
 function assembleAndCreate(header, body, footer, fn, zs){
 	return new Promise((resolve,reject)=>{
+
+
 		var fullDocString = header.join('') + body.join('') + footer.join('');
 		ctt.parseText(fullDocString, zs).then((buff)=>{
 			fs.writeFile(fn, buff,(err)=>{
@@ -108,6 +110,36 @@ function assembleAndCreate(header, body, footer, fn, zs){
 				reject(">>> ERROR: FILE konnte nicht erstellt werden");
 			});
 		},reject)
+
+
+		//------------------------------------------------------------------
+		var ibmSetstring = "P,`,p,á,!,1,A,Q,a,q,ß,2,2,B,R,b,r,é,3,3,#,3,C,S,c,s,â,4,4,$,4,D,T,d,t,ä,ö,5,5,§,%,5,E,U,e,u,à,6,6,&,6,F,V,f,v,μ,7,7,',7,G,W,g,w,ç,8,8,(,8,H,X,h,x,ê,°,9,9,),9,I,Y,i,y,Ö,10,A,*,:,J,Z,j,z,è,Ü,11,B,+,;,K,[,k,½,12,C,<,L,l,13,D,-,=,M,],m,14,E,.,>,N,^,n,Ä,15,F,/,?,O,o"
+		var iso88591SetString = "0,0,0,@,P,`,p,°,à,1,1,!,1,A,Q,a,q,¡,Ñ,á,ñ,2,2,2,B,R,b,r,â,ò,3,3,#,3,C,S,c,s,ó,4,4,$,4,D,T,d,t,Ä,ä,ô,5,5,%,5,E,U,e,u,μ,Å,å,6,6,&,6,F,V,f,v,Æ,Ö,æ,ö,7,7,',7,G,W,g,w,§,Ç,ç,8,8,(,8,H,X,h,x,è,9,9,),9,I,Y,i,y,É,é,ù,10,A,LF,*,:,J,Z,j,z,º,ê,ú,11,B,+,;,K,[,k,ë,û,12,C,,,<,L,l,Ü,ì,ü,13,D,-,=,M,],m,½,í,14,E,.,>,N,^,n,î,15,F,/,?,O,_,o,ß,ï,"
+		var iso885015SetString = "0,0,0,@,P,`,p,°,À,Ð,à,ð,1,1,!,1,A,Q,a,q,¡,±,Á,Ñ,á,ñ,2,2,2,B,R,b,r,¢,²,Â,Ò,â,ò,3,3,#,3,C,S,c,s,£,³,Ã,Ó,ã,ó,4,4,$,4,D,T,d,t,€,Ž,Ä,Ô,ä,ô,5,5,%,5,E,U,e,u,¥,μ,Å,Õ,å,õ,6,6,&,6,F,V,f,v,Š,¶,Æ,Ö,æ,ö,7,7,',7,G,W,g,w,§,·,Ç,×,ç,÷,8,8,(,8,H,X,h,x,š,ž,È,Ø,è,ø,9,9,),9,I,Y,i,y,©,¹,É,Ù,é,ù,10,A,*,:,J,Z,j,z,ª,º,Ê,Ú,ê,ú,11,B,+,;,K,[,k,{,«,»,Ë,Û,ë,û,12,C,,,<,L,\,l,|,¬,OE,Ì,Ü,ì,ü,13,D,CR,-,=,M,],m,},oe,Í,Ý,í,ý,14,E,.,>,N,^,n,~,®,Ÿ,Î,Þ,î,þ,15,F,/,?,O,_,o,¯,¿,Ï,ß,ï"
+		
+		ctt.parseText(ibmSetstring, 2).then((buff)=>{
+			fs.writeFile("./ibmBeweis", buff,()=>{
+				console.log("IBM File wurde erstellt");
+			},reject)
+		},()=>{
+			console.log("IBM File konnte nicht erstellt werden");
+		})
+		ctt.parseText(iso88591SetString, 3).then((buff)=>{
+			fs.writeFile("./Iso8851-1", buff,()=>{
+				console.log("ISO8851 File wurde erstellt");
+			},reject)
+		},()=>{
+			console.log("ISO8851 File konnte nicht erstellt werden");
+		})
+		ctt.parseText(iso885015SetString, 4).then((buff)=>{
+			fs.writeFile("./Iso8859-15", buff,()=>{
+				console.log("Iso8859-15 File wurde erstellt");
+			},reject)
+		},()=>{
+			console.log("Iso8859-15 File konnte nicht erstellt werden");
+		})
+		//------------------------------------------------------------------
+
 	});
 }
 function isValidPath(x){
